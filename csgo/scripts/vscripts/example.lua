5945537bae5233877a3f019b84583a04c1118c02
Nick Fox <66554932+NickFox007@users.noreply.github.com>
2023-03-29 01:58:27 +0300

    First release

--- csgo/scripts/vscripts/example.lua
+++ csgo/scripts/vscripts/example.lua
@@ -1,3 +1,3 @@
 -- LOADER_SETSCRIPTINFO(script_name, script_description, script_version, script_author)
 
-LOADER_SETSCRIPTINFO("Example Lua", "Shows you example for registering script", "1.0", "Nick Fox")
+if LOADER_SETSCRIPTINFO ~= nil then LOADER_SETSCRIPTINFO("Example Lua", "Shows you example for registering script", "1.0", "Nick Fox") end
